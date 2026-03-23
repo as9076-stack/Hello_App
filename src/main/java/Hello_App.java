df2a3877d2477ec4fecf0f2a0f757e427ef9276f
as9076 <as9076@srmist.edu.in>
2026-03-23 10:15:41 +0530

    Coded UC2: Display Hello with Command-Line Argument

--- src/main/java/Hello_App.java
+++ src/main/java/Hello_App.java
@@ -2,5 +2,9 @@ public class Hello_App
 {
     public static void main(String[] args){
         System.out.println("Hello World");
+
+        String name= args[0];
+        System.out.println("Hello, "+name+"!");
     }
+
 }
